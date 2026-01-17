d8b88f20537d22976186ca7a3201422a9fa78f59
김나현 <skgus0423@naver.com>
2026-01-17 15:46:19 +0900

    feat: 백엔드 초기 세팅

---
+++ server/src/main/java/com/project/kkookk/KkookkApplication.java
@@ -0,0 +1,13 @@
+package com.project.kkookk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkookkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkookkApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/project/kkookk/KkookkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kkookk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkookkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
